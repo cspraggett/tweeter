8a8456686b08a4a5d2975e05775fa899ac56ca4a
Christopher Spraggett <cspraggett@gmail.com>
2020-01-29 15:17:52 -0500

    Add get request with ajax to populate the form from the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,37 +1,52 @@
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
+
+const submitWithAjax = function() {
+  const $form = $('main form');
+  console.log('found the button');
+  $form.submit(function(event) {
+    event.preventDefault();
+    console.log('Button clicked, performing ajax call...');
+    $.ajax('/tweets/', {method: 'POST', data: $(this).serialize() });
+    // .then(renderTweets());
+  });
+};
+
+const loadtweets = function() {
+  console.log('Loading tweets');
+  $.ajax({url: '/tweets', method: 'GET', dataType: 'JSON'})
+    .then(response => {
+      renderTweets(response);
+    });
+};
 
 const getDate = ((date) => {
   const today = new Date();
   const postDate = new Date(date);
   const diff = (today.getTime() - postDate.getTime()) / (1000 * 60 * 60 * 24);
-
-
-  
-  // console.log(`${diff}`);
   return Math.floor(diff);
 });
 
@@ -50,6 +65,9 @@ const renderTweets = (tweets => {
 });
 
 $(document).ready(() => {
-  renderTweets(data);
+  loadtweets();
+  submitWithAjax();
+
+  //renderTweets(data);
   //$('#container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
