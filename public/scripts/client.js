9e7476f4b78f74dba3bdca89ed57ea31f9c2f94c
Christopher Spraggett <cspraggett@gmail.com>
2020-01-29 13:06:25 -0500

    Fixed css and implemented dynamic rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
+const getDate = ((date) => {
+  const today = new Date();
+  const postDate = new Date(date);
+  const diff = (today.getTime() - postDate.getTime()) / (1000 * 60 * 60 * 24);
+
+
+  
+  // console.log(`${diff}`);
+  return Math.floor(diff);
+});
+
+const createTweetElement = (tweetData => {
+  const {name, avatars, handle} = tweetData.user;
+  const {text} = tweetData.content;
+  const {created_at} = tweetData;
+  getDate(created_at);
+  return `<article class="tweet"><header><span><img src="${avatars}"> ${name}</span><span class="userName">${handle}</span></header><section class="tweet"><p>${text}</p></section><footer><span>${getDate(created_at)} days ago</span><span class="links"><a href="/">Various links</a> </span></footer></article>`;
+});
+
+//const $tweet = createTweetElement(data);
+
+// Test / driver code (temporary)
+// console.log('this is the tweet:', $tweet); // to see what it looks like
+
+const renderTweets = (tweets => {
+  for (const tweet of tweets) {
+    $('#container').append(createTweetElement(tweet));
+  }
+});
+
+$(document).ready(() => {
+  renderTweets(data);
+  //$('#container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
