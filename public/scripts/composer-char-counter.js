0821f436ec22725b97290eeaf43b61d4f5d2ada7
Christopher Spraggett <cspraggett@gmail.com>
2020-01-30 20:20:39 -0500

    cleaned up loose comments on css files

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,5 @@
+// checks both the keyUp and keyDown of the new-tweet textarea and updates the character counter.
+// There is a bug... It won't update the character count when text is pasted into the textarea using the mouse.+
 $(document).ready(function() {
   $(".new-tweet textarea").on('keydown', function() {
     let charCount = $(this).val().length;
